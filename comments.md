a8329afd0655829ba412e60de5f141f5dcd85a94
Aleksey-Dr <aprel181818@gmail.com>
2023-05-10 20:54:50 +0300

    Step 1 Adding contact name and show list contacts
    1. Added a markup to the render() of "App" component-class.
    2. Added the values to the "state" of the "App" component-class.
    3. Added <label> "Name" to the markup in the render() of the "App" component-class.
    4. Added <button type="submit"> to the markup in the render() of the "App" component-class.
    5. Add "htmlFor={ this.nameInput }" to the <label>.
    6. Add "value={ this.state.name }" to the <input>.
    7. Add a "handleChange" method to the "App" component-class.
    8. Add "import shortid from 'shortid';" to the App.jsx.
    9. Add an "addContact" method to the "App"component-class - create an object with two elements: 1) id: shortid.generate
       (), 2) name: this.currentTarget.value.
    10. Add a "handleSubmit" method to the "App" component-class.
    11. Add "event.preventDefault();" to the "handleSubmit" method.
    12. Add block "Contacts" to the markup in the render() of the "App" component-class.
    13. Add render <li> items by map() in the block "Contacts".
    
    TOTAL:
    1. The Phonebook app must consists of a "form" and a "contacts list".
    2. Adding a contact name is implemented at the current step.
    3. The app didn't must save contacts between different sessions (updata page).
    4. Used markup of template includes validate for contact name.
    5. The "state" is corresponds template.
    6. Each "contact" is an object with values: "name" and "id".
    7. For generate "id" used "shortid".

--- comments.md
+++ comments.md
@@ -14,3 +14,28 @@ Commons settings
 5. Added "extends Component" for "App" component-class
 6. Add "state" to the "App" component-class.
 7. Add the method render() to the "App" component-class.
+
+Step 1 "Adding name of a contact and show list contacts"
+1. Added a markup to the render() of "App" component-class.
+2. Added the values to the "state" of the "App" component-class.
+3. Added <label> "Name" to the markup in the render() of the "App" component-class.
+4. Added <button type="submit"> to the markup in the render() of the "App" component-class.
+5. Add "htmlFor={ this.nameInput }" to the <label>.
+6. Add "value={ this.state.name }" to the <input>.
+7. Add a "handleChange" method to the "App" component-class.
+8. Add "import shortid from 'shortid';" to the App.jsx.
+9. Add an "addContact" method to the "App"component-class - create an object with two elements: 1) id: shortid.generate
+   (), 2) name: this.currentTarget.value.
+10. Add a "handleSubmit" method to the "App" component-class.
+11. Add "event.preventDefault();" to the "handleSubmit" method.
+12. Add block "Contacts" to the markup in the render() of the "App" component-class.
+13. Add render <li> items by map() in the block "Contacts".
+
+TOTAL:
+1. The Phonebook app must consists of a "form" and a "contacts list".
+2. Adding a contact name is implemented at the current step.
+3. The app didn't must save contacts between different sessions (updata page).
+4. Used markup of template includes validate for contact name.
+5. The "state" is corresponds template.
+6. Each "contact" is an object with values: "name" and "id".
+7. For generate "id" used "shortid".
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,35 @@
 import { Component } from 'react';
+import shortid from 'shortid';
 
 export class App extends Component {
   state = {
     contacts: [],
     name: ''
+  };
+
+  addContact = name => {
+    const contact = {
+      id: shortid.generate(),
+      name,
+    };
+
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  handleChange = event => {
+    this.setState({
+      name: event.currentTarget.value,
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.addContact(this.state.name);
+
+    this.setState({ name: '' });
   };
 
   render() {
@@ -18,9 +44,36 @@ export class App extends Component {
       //   color: '#010101'
       // }}
       >
-        
+        <h2>Phonebook</h2>
+
+        <form onSubmit={ this.handleSubmit }>
+          <label htmlFor={ this.nameInput }>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+              value={ this.state.name }
+              onChange={ this.handleChange }
+          />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+
+        <div>
+          <h3>Contacts</h3>
+          <ul>
+            {this.state.contacts.map(({ id, name }) => {
+              return (<li key={id}>
+                <p>{name}</p>
+              </li>);
+            })}
+          </ul>
+        </div>
       </div>
     );
-  }
+  };
   
 };
